caf7d1835a78e50e6ed020db6d5ced273af75dab
Longwu Ou <xulongwu4@gmail.com>
2020-02-27 11:53:21 -0500

    Simplify the code to find the extra_actions dir

--- kythe/generate_compile_commands/generate_compilation_database.sh
+++ kythe/generate_compile_commands/generate_compilation_database.sh
@@ -18,15 +18,9 @@ bazel build \
   --noshow_loading_progress \
   $@ > /dev/null
 
-WORKSPACE=$(bazel info workspace)
+WORKSPACE=$(bazel info workspace 2>/dev/null)
 OUTFILE=$WORKSPACE/compile_commands.json
-KYTHE_WORKSPACE=$(bazel aquery \
-  --experimental_action_listener=//kythe/generate_compile_commands:extract_json \
-  --nosandbox_debug \
-  $@ 2> /dev/null \
-  | rg Outputs | rg /bin/ | tail -1 \
-  | cut -f 2 -d: | sed 's/[][]//g' | awk '{$1=$1};1')
-KYTHE_WORKSPACE=${KYTHE_WORKSPACE%/bin/*}/extra_actions/kythe/generate_compile_commands
+KYTHE_WORKSPACE=$(bazel info bazel-bin 2>/dev/null)/../extra_actions/kythe/generate_compile_commands
 
 python3 $(dirname $0)/generate_compile_commands_json.py $WORKSPACE $KYTHE_WORKSPACE
 jq . $OUTFILE > $WORKSPACE/formatted.json && mv $WORKSPACE/formatted.json $OUTFILE
