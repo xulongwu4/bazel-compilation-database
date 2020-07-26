bfa490389575204ccaa92e88bb651b0a569b3bb2
Longwu Ou <xulongwu4@gmail.com>
2020-07-26 10:08:09 -0400

    Formatting

--- kythe/generate_compile_commands/generate_compilation_database.sh
+++ kythe/generate_compile_commands/generate_compilation_database.sh
@@ -13,21 +13,21 @@ BAZEL_ROOT=$(bazel info execution_root 2>/dev/null)
 [ -d $KYTHE_WORKSPACE ] && find $KYTHE_WORKSPACE -name '*.compile_command.json' -delete
 
 bazel build \
-  --color=yes \
-  --experimental_action_listener=//kythe/generate_compile_commands:extract_json \
-  --nosandbox_debug \
-  --noshow_progress \
-  --noshow_loading_progress \
-  $(bazel query 'kind(cc_.*, //...) - attr(tags, manual, //...) - //kythe/...' 2>/dev/null) > /dev/null
+    --color=yes \
+    --experimental_action_listener=//kythe/generate_compile_commands:extract_json \
+    --nosandbox_debug \
+    --noshow_progress \
+    --noshow_loading_progress \
+    $(bazel query 'kind(cc_.*, //...) - attr(tags, manual, //...) - //kythe/...' 2>/dev/null) >/dev/null
 
-echo "[" > $OUTFILE
-find $KYTHE_WORKSPACE -name '*.compile_command.json' -exec cat {} + >> $OUTFILE
-echo -e "\n]" >> $OUTFILE
+echo "[" >$OUTFILE
+find $KYTHE_WORKSPACE -name '*.compile_command.json' -exec cat {} + >>$OUTFILE
+echo -e "\n]" >>$OUTFILE
 sed -i "s|@BAZEL_ROOT@|$BAZEL_ROOT|g" $OUTFILE
 sed -i "s/}{/},\n{/g" $OUTFILE
 
 # Use `jq` to format the compilation database
 if hash jq 2>/dev/null; then
     TMPFILE=$(mktemp)
-    jq . $OUTFILE > $TMPFILE && mv $TMPFILE $OUTFILE || rm $TMPFILE
+    jq . $OUTFILE >$TMPFILE && mv $TMPFILE $OUTFILE || rm $TMPFILE
 fi
