f47de455e69440ff16b6f820278b7fece5f5d5ec
Longwu Ou <longwu.ou@lightelligence.ai>
2020-09-25 10:47:04 -0400

    Update python-based method

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## Usage
 
-Copy (or symlink) the kythe directory to the workspace of your project. Then add
+### Approach 1: `cpp`-based method
+
+Copy (or symlink) the `kythe` directory to the workspace of your project. Then add
 the following lines to the `WORKSPACE` file:
 
 ```bzl
@@ -20,3 +22,13 @@ kythe/generate_compile_commands/generate_compilation_database.sh
 
 It is highly recommended to have [`jq`](https://github.com/stedolan/jq) installed
 so that the generated `compile_commands.json` is formatted nicely.
+
+### Approach 2: `python`-based method
+
+Copy (or symlink) the `tools` directory to the workspace of your project. Then
+run the python script `tools/generate_compilation_database.sh` in the workspace
+directory to generate the compilation database:
+
+```sh
+tools/generate_compilation_database.sh ...
+```
---
+++ tools/generate_compilation_database.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env sh
+
+# Generates a compile_commands.json file at $(bazel info execution_root) for
+# your clang tooling needs.
+
+printError() {
+    printf '\033[31mERROR:\033[0m %s\n' "$@"
+}
+
+printInfo() {
+    printf '\033[32mINFO:\033[0m %s\n' "$@"
+}
+
+log_err() {
+    printError "See log file at $LOGFILE"
+}
+
+set -e
+
+trap '[ $? -eq 0 ] || log_err' EXIT
+
+LOGFILE=$(mktemp)
+
+printInfo "Log file: $LOGFILE"
+
+printInfo "Fetching workspace info ..."
+
+TOOLS_WORKSPACE=$(bazel info bazel-bin 2>>"$LOGFILE")/../extra_actions/tools/generate_compile_commands_action
+
+[ -d "$TOOLS_WORKSPACE" ] && find "$TOOLS_WORKSPACE" -name '*_compile_command' -delete
+
+printInfo "Querying build targets ..."
+BUILD_TARGETS=$(bazel query 'kind(cc_.*, //...) - attr(tags, manual, //...) - //tools/...' 2>>"$LOGFILE")
+
+printInfo "Building compilation database ..."
+# Do not use double quotes around $BUILD_TARGETS, so shell will perform field splitting and remove newlines between
+# build targets
+bazel build \
+    --color=yes \
+    --experimental_action_listener=//tools:generate_compile_commands_listener \
+    --nosandbox_debug \
+    --noshow_progress \
+    --noshow_loading_progress \
+    $BUILD_TARGETS >>"$LOGFILE" 2>&1
+
+python3 "$(dirname "$0")"/generate_compile_commands_json.py >>"$LOGFILE" 2>&1
+
+if hash jq 2>/dev/null; then
+    # Use `jq` to format the compilation database
+    printInfo "Formatting compilation database ..."
+    TMPFILE=$(mktemp)
+    WORKSPACE=$(bazel info workspace 2>"$LOGFILE")
+    OUTFILE=$WORKSPACE/compile_commands.json
+    jq . "$OUTFILE" >"$TMPFILE" && cp --no-preserve=mode "$TMPFILE" "$OUTFILE" && rm "$TMPFILE"
+else
+    printInfo "Can not find jq. Skip formatting compilation database."
+fi
+
+rm "$LOGFILE"
